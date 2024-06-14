1a93e2a04e977c7fb50ee18330ee08be0174ed90
Muhammad Husnain <bitsol44@gmail.com>
2024-06-14 15:13:07 +0500

    Update database.py

--- app/services/database.py
+++ app/services/database.py
@@ -4,7 +4,6 @@ import asyncio
 from app.services.logs import logger
 from app.constants.config import mongo_url
 from app.models.db_models.user import User
-<<<<<<< HEAD
 
 async def init_db():
     try:
@@ -12,7 +11,7 @@ async def init_db():
         client = motor.motor_asyncio.AsyncIOMotorClient(mongo_url)
         client.get_io_loop = asyncio.get_running_loop
         await init_beanie(database=client[database_name], document_models=[User])
-=======
+
 from app.models.db_models.fileScan import FileScan
 from app.models.db_models.urlScan import UrlScan
 
@@ -22,7 +21,7 @@ async def init_db():
         client = motor.motor_asyncio.AsyncIOMotorClient(mongo_url)
         client.get_io_loop = asyncio.get_running_loop
         await init_beanie(database=client[database_name], document_models=[User, FileScan, UrlScan])
->>>>>>> c18931cd379c94ab8ef653a2d07b3d6f15040e9e
+
         logger.info('Database connected.')
     except Exception as err:
         logger.error(f'Database connection failed, error is: {err}')
