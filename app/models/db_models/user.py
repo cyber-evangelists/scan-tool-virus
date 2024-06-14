d6943e3ae91424e5f80928417d2f7cfd252c1d6b
Muhammad Husnain <bitsol44@gmail.com>
2024-06-14 15:11:48 +0500

    Update user.py

--- app/models/db_models/user.py
+++ app/models/db_models/user.py
@@ -6,13 +6,12 @@ class User(Document):
     first_name: str
     last_name: str
     email: str
-<<<<<<< HEAD
+
     role: str
 
     class Settings:
         name = "users"
-=======
 
     class Settings:
         name = "Users"
->>>>>>> c18931cd379c94ab8ef653a2d07b3d6f15040e9e
+
